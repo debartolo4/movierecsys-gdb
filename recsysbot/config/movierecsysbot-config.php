a63cad80f9cc06ad336391f5c00d17c204182b67
debartolo4 <g.debartolo4@studenti.uniba.it>
2018-02-14 21:39:12 +0100

    hope

--- recsysbot/config/movierecsysbot-config.php
+++ recsysbot/config/movierecsysbot-config.php
@@ -16,9 +16,9 @@ return [
 		// Facebook payload returned from "Start" button
 		'getStartedPayload' => "get_started",
         // Base URI (Altieri)
-        'base_uri' => '127.0.0.1:8080', //MODIFICABILE
+        'base_uri' => 'https://214d9de3.ngrok.io -> localhost:80', //MODIFICABILE
 		// Server application URI (Altieri)
-		'application_uri' => '/movierecsysrestful-old', //MODIFICABILE
+		'application_uri' => '/movierecsysservice-0.0.1-SNAPSHOT', //MODIFICABILE
 		// Default photo to send if the provided one is not valid
 		'default_photo' => 'https://bytebucket.org/lu_na/movierecsystelegram/raw/722f6bf618a3d9342c678c878ee60a593d08721b/recsysbot/images/default.jpg?token=8b07161012a8adb6134efa50aa5c98a2b0bd4ba0'
 ];
