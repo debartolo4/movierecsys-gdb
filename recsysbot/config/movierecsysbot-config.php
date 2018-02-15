77329090085d4f625cd246349952bb739c91faac
debartolo4 <g.debartolo4@studenti.uniba.it>
2018-02-15 12:36:14 +0100

    jjj

--- recsysbot/config/movierecsysbot-config.php
+++ recsysbot/config/movierecsysbot-config.php
@@ -18,7 +18,7 @@ return [
         // Base URI (Altieri)
         'base_uri' => 'http://f1816d11.ngrok.io/localhost:8080', //MODIFICABILE
 		// Server application URI (Altieri)
-		'application_uri' => '/movierecsysrestful', //MODIFICABILE
+		'application_uri' => '/movierecsysrestful2', //MODIFICABILE
 		// Default photo to send if the provided one is not valid
 		'default_photo' => 'https://bytebucket.org/lu_na/movierecsystelegram/raw/722f6bf618a3d9342c678c878ee60a593d08721b/recsysbot/images/default.jpg?token=8b07161012a8adb6134efa50aa5c98a2b0bd4ba0'
 ];
