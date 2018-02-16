1da604b169d1bfca1fbc743a153525fefc25e0b0
debartolo4 <g.debartolo4@studenti.uniba.it>
2018-02-16 22:28:03 +0100

    hbhbh

--- recsysbot/config/movierecsysbot-config.php
+++ recsysbot/config/movierecsysbot-config.php
@@ -16,7 +16,7 @@ return [
 		// Facebook payload returned from "Start" button
 		'getStartedPayload' => "get_started",
         // Base URI (Altieri)
-        'base_uri' => 'https://2e1d3550.ngrok.io/localhost:8080', //MODIFICABILE
+        'base_uri' => 'https://5b457c36.ngrok.io/localhost:8080', //MODIFICABILE
 		// Server application URI (Altieri)
 		'application_uri' => '/movierecsysservice', //MODIFICABILE
 		// Default photo to send if the provided one is not valid
